8009857698ea485f35cdab5b354d33679161c1d1
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-08 00:57:35 +0000

    head function

--- head.js
+++ head.js
@@ -7,10 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, 1);
-assertEqual("Lighthouse", "lighthouse");
-assertEqual("Labs", "Labs");
-assertEqual("4", "4");
+function head (array){
+  return array[0]
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), undefined);
