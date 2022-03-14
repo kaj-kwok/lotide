e47ac6d0df416fd25998c9eadc08beed1cc1f8e3
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-14 16:03:11 -0700

    refactored middle.js added test file for middle

--- middle.js
+++ middle.js
@@ -1,26 +1,5 @@
-const eqArrays = function(arr1, arr2){
-  if(arr1.length !== arr2.length){
-    return false
-  }
-  if(arr1 == null || arr2 == null){
-    return false
-  }
-  for(let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      return false
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (actual, expected){
-  if(actual === expected){
-    console.log(`✔️✔️✔️ Assertion Passed: ${actual} matches ${expected}`);
-  }
-  else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function(array){
   let middleArray = []
@@ -37,12 +16,5 @@ const middle = function(array){
   }
 }
 
-// test assertions
-// console.log(middle([1, 2, 3]))
-// console.log(middle([1, 2, 3, 4, 5]))
+module.exports = middle
 
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2,3]), true)
-assertArraysEqual(eqArrays(middle([1, 2, 3]), [2,3]), true)
