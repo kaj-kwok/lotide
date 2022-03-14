c57db27dacd6d54be6264eaa3973487e1722a547
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-14 15:57:04 -0700

    refactored assertArrayEquals added test file for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = function(arr1, arr2){
-  if(arr1.length !== arr2.length){
-    return false
-  }
-  if(arr1 == null || arr2 == null){
-    return false
-  }
-  for(let i = 0; i < arr1.length; i++){
-    if (arr1[i] !== arr2[i]){
-      return false
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected){
   if(eqArrays(actual, expected)){
@@ -22,7 +9,4 @@ const assertArraysEqual = function(actual, expected){
   }
 }
 
-assertArraysEqual([1,2,3], [1,2,3])
-// assertArraysEqual([1,2,3], [1,2,3])
-// assertArraysEqual([1,2,3,5], [1,2,3])
-// assertArraysEqual([1,2,3], ["1",2,3])
+module.exports = assertArraysEqual
