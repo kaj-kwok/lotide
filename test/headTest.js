05cec39930d97529205ee1b0c8d53864be759d6c
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-14 16:46:29 -0700

    modified headtest to use chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,17 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("return 'hello' for array ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+  });
+  it("return undefined for array []", () => {
+    assert.strictEqual(head([]), undefined)
+  });
+});
