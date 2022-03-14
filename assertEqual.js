7e41bb29ba6a0b04d415eb4e666d6a17aa070b2c
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-14 15:22:20 -0700

    refactored assertequals and head, created test cases

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, 1);
-assertEqual("Lighthouse", "lighthouse");
-assertEqual("Labs", "Labs");
-assertEqual("4", "4");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 1);
+assertEqual("Lighthouse", "lighthouse");
+assertEqual("Labs", "Labs");
+assertEqual("4", "4");
