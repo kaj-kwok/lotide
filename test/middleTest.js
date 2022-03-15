c8bd91fd7ca6e7e85baae1f2fd9c8a32de9beb50
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-14 17:11:12 -0700

    updated tailtest to include chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -15,6 +15,3 @@ describe("testing that middle function works", () => {
     assert.deepEqual(middle([1]), [])
   });
 });
-
-// assertArraysEqual(middle([1, 2, 3, 4]), [2,3])
-// assertArraysEqual(middle([1, 2, 3]), [2,3])
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,14 @@
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const words1 = tail(["Hello"]); // test with one element
-assertEqual(words1.length, 1);
-
-const words2 = tail([]); // test with zero  element
-assertEqual(words2.length, 0);
+describe("test values for using tail function", () => {
+  it("return should be ['Lighthouse', 'Labs'] for array ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+  it("return should be [] for array ['1']", () => {
+    assert.deepEqual(tail(["1"]), [])
+  });
+  it("return should be [] for array []", () => {
+    assert.deepEqual(tail([]), [])
+  });
+})
